f6de0364581c6b9dca233096fa2faba86569ae51
Nicola Puoti <np84digitalproduct@gmail.com>
2026-02-23 18:11:55 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,3 +3,7 @@ terraform {
     bucket         = "tfstate-eks-lab-nick84667-eu-central-1"
     key            = "eks-lab/terraform.tfstate"
     region         = "eu-central-1"
+    dynamodb_table = "terraform-locks-eks-lab"
+    encrypt        = true
+  }
+}
